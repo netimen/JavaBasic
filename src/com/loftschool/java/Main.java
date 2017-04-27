f163730d49eb16631a6ad11b4c651e8d6eda870f
Dmitry Gordeev <netimen@gmail.com>
2017-04-27 19:03:12 +0300

    methods lesson

--- src/com/loftschool/java/Main.java
+++ src/com/loftschool/java/Main.java
@@ -2,32 +2,48 @@ package com.loftschool.java;
 
 public class Main {
 
-    public static void main(String[] args) {
-        boolean isGood = true;
-        if (isGood)
-            System.out.println("do something good");
-        else
-            System.out.println("do something bad");
-        System.out.println("do something fun");
-
-        boolean otherCondition = false;
-        if (isGood && otherCondition) {
-            System.out.println("do something good");
-            System.out.println("do something very good");
-        }
-
-        int repeatCount = 2;
-        while (repeatCount-- > 0)
-            System.out.println("do something while...");
-
-        do
-            System.out.println("do something while...");
-        while (repeatCount-- > 0);
-
-        for (int i = 0; i < 10; i++)
-            System.out.println("iteration number " + i);
-
-        for (int i = 0; i < 10; i--)
-            System.out.println("INFINITE LOOP!!!");
+    static void printPrice(int price) {
+        System.out.println("Price is " + price);
+        System.out.println("Our price is the best");
     }
+
+    static int applyDiscount(int price, int discountPercent) {
+        int newPrice = price * (100 - discountPercent) / 100;
+        return newPrice;
+    }
+
+    static int applyDiscount(int price, int discountPercent, boolean oldClient) {
+        percent++; // ERROR
+        if (oldClient)
+            discountPercent *= 2;
+
+        return applyDiscount(price, discountPercent);
+    }
+
+    public static void main(String[] args) {
+        int mySalary = 5000;
+        if (mySalary > 1000) {
+            int myMoney = mySalary;
+            myMoney -= 500; // tickets to India
+            if (myMoney > 500) {
+                myMoney -= 100; // new shoes
+                myMoney -= 10; // new T-shirt
+            }
+        }
+        myMoney -= 200; // ERROR
+
+        System.out.println("Price is " + 30);
+        System.out.println("Our price is the best");
+
+        System.out.println("Price is " + 50);
+        System.out.println("Our price is the best");
+
+        printPrice(30);
+        printPrice(50);
+
+        int price = 30, percent = 25;
+        int myPrice = applyDiscount(price, percent, true);
+        System.out.println(percent); // still 25!!!
+    }
+
 }
